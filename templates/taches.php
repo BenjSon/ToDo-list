318f43cbb64810fc120e22a1fa85bef035ae17b8
asmchampion <paulhenri.pinet@gmail.com>
2018-06-21 15:45:07 +0200

    v 0.3.8

--- templates/taches.php
+++ templates/taches.php
@@ -6,39 +6,7 @@ if (basename($_SERVER["PHP_SELF"]) == "conversations.php")
 {
 	header("Location:../index.php?view=conversations");
 	die("");
-}
-
-include_once("libs/modele.php"); // listes
-include_once("libs/maLibUtils.php");// tprint
-include_once("libs/maLibForms.php");// mkTable, mkLiens, mkSelect ...
-
-
-
-?>
-
-<?php
-// Si l'utilisateur n'est pas connecte, on affiche un lien de connexion
-if (!valider("connecte","SESSION"))
-    echo "<a href=\"index.php?view=login\">Se connecter</a>";
-?>
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<!-- **** H E A D **** -->
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
-    <title>TinyMVC</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<!-- **** F I N **** H E A D **** -->
-
-
-<!-- **** B O D Y **** -->
-<body>
-
-
+}?>
     <div>Ajouter une tache</div>
     <form action="/ajout tâche" method="post">
         <div>
@@ -52,7 +20,7 @@ if (!valider("connecte","SESSION"))
 
         <label for="matière" >Dans quelle matière ?</label>
         <select name="matières" id="matières" required>
-            <option value="france">France</option>
+            <option value="français">France</option>
             <option value="espagne">Espagne</option>
             <option value="etats-unis">États-Unis</option>
             <option value="chine">Chine</option>
@@ -72,7 +40,3 @@ if (!valider("connecte","SESSION"))
 
     </form>
 </div>
-</body>
-</html>
-
-
