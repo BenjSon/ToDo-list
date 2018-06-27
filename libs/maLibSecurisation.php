3db4f213ad2415f5b413057b0106e736cfcb461a
asmchampion <paulhenri.pinet@gmail.com>
2018-06-27 09:27:42 +0200

    verif user

--- libs/maLibSecurisation.php
+++ libs/maLibSecurisation.php
@@ -21,30 +21,21 @@ include_once "modele.php";	// Car on utilise la fonction connecterUtilisateur()
  */
 function verifUser($login,$password)
 {
-	// OK : on crée les var. de session
-		$_SESSION["pseudo"] = $login; 
-		$_SESSION["idUser"] = $idUser;
+    // Appelle une fonction de vérification des identifiants de la couche modele
+    // nommée verifUserBdd($login,$passe) ainsi que la fonction isAdmin($idUser)
 
-		$_SESSION["heureConnexion"] = date("H:i:s"); 
-		$_SESSION["connecte"] = true;
-		return true;
+    // vérifie si le login/passe passés en paramètre sont légaux
+    // renvoie faux ou un identifiant
+    // (qui sera un entier strictement positif)
 
-}
-
-
-
-
-
-/**
- * Fonction à placer au début de chaque page privée
- * Cette fonction redirige vers la page $urlBad en envoyant un message d'erreur 
-	et arrête l'interprétation si l'utilisateur n'est pas connecté
- * Elle ne fait rien si l'utilisateur est connecté, et si $urlGood est faux
- * Elle redirige vers urlGood sinon
- */
-function securiser($urlBad,$urlGood=false)
-{
-
+    if ($idUser = verifUserBdd($login,$password)) {
+        // OK : on crée les var. de session
+        $_SESSION["pseudo"] = $login;
+        $_SESSION["idUser"] = $idUser;
+        $_SESSION["heureConnexion"] = date("H:i:s");
+        $_SESSION["connecte"] = true;
+        return true;
+    } else return false;
 }
 
 ?>
